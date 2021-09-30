c0df634195384e5983b484424a1766579a5c683d
twistedrwb <90223581+twistedrwb@users.noreply.github.com>
2021-09-30 16:20:46 -0300

    Add files via upload

--- PrimeiroJogo/js/script.js
+++ PrimeiroJogo/js/script.js
@@ -1,10 +1,26 @@
 window.onload = function(){
     iniciarJogo();
-    entidadeJogador = entidadeJogador('#000', 50, 50, 50, 200);
+
+    document.querySelector("#Up").addEventListener("click", function(){
+        moveUp();
+    });
+    document.querySelector("#Left").addEventListener("click", function(){
+        moveLeft();
+    });
+    document.querySelector("#Right").addEventListener("click", function(){
+        moveRight();
+    });
+    document.querySelector("#Down").addEventListener("click", function(){
+        moveDown();
+    });
+
 }
+
+var entJogador;
 
 function iniciarJogo(){
     espacoJogo.inicio();
+    entJogador = new proprJogador('#000', 50, 50, 50, 200);
 }
 
 let espacoJogo = {
@@ -14,15 +30,50 @@ let espacoJogo = {
         this.canvas.height = 500,
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updtEspacoJogo, 20);
+    },
+    clearSpace: function(){
+        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
     }
 }
 
-function entidadeJogador(cor, largura, altura, x, y){
+function proprJogador(cor, largura, altura, x, y){
     this.altura = altura,
     this.largura = largura,
     this.x = x,
     this.y = y,
-    contexto = espacoJogo.context;
-    contexto.fillStyle = cor;
-    contexto.fillRect(this.x, this.y, this.altura, this.largura);
+    this.speedX = 0,
+    this.speedY = 0,
+    this.updtPosition = function(){
+        contexto = espacoJogo.context;
+        contexto.fillStyle = cor;
+        contexto.fillRect(this.x, this.y, this.altura, this.largura);
+    }
+    this.newPosition = function(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
 }
+
+function updtEspacoJogo(){
+    espacoJogo.clearSpace();
+    entJogador.newPosition();
+    entJogador.updtPosition();
+}
+
+function moveUp(){
+    entJogador.speedY -= 1;
+}
+
+function moveDown(){
+    entJogador.speedY += 1;
+}
+
+function moveLeft(){
+    entJogador.speedX -= 1;
+}
+
+function moveRight(){
+    entJogador.speedX += 1;
+}
+
