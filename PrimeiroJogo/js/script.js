2ac254b055e1cfac3507c8a3fbb05dcf16953a01
twistedrwb <90223581+twistedrwb@users.noreply.github.com>
2021-10-01 11:43:33 -0300

    Add files via upload

--- PrimeiroJogo/js/script.js
+++ PrimeiroJogo/js/script.js
@@ -1,26 +1,41 @@
 window.onload = function(){
     iniciarJogo();
-
-    document.querySelector("#Up").addEventListener("click", function(){
+    //Movimentação e direção
+    document.querySelector("#Up").addEventListener("mousedown", function(){
         moveUp();
     });
-    document.querySelector("#Left").addEventListener("click", function(){
+    document.querySelector("#Left").addEventListener("mousedown", function(){
         moveLeft();
     });
-    document.querySelector("#Right").addEventListener("click", function(){
+    document.querySelector("#Right").addEventListener("mousedown", function(){
         moveRight();
     });
-    document.querySelector("#Down").addEventListener("click", function(){
+    document.querySelector("#Down").addEventListener("mousedown", function(){
         moveDown();
     });
 
+    //Freio na movimentação
+    document.querySelector("#Up").addEventListener("mouseup", function(){
+        movemntBrake();
+    });
+    document.querySelector("#Left").addEventListener("mouseup", function(){
+        movemntBrake();
+    });
+    document.querySelector("#Right").addEventListener("mouseup", function(){
+        movemntBrake();
+    });
+    document.querySelector("#Down").addEventListener("mouseup", function(){
+        movemntBrake();
+    });
 }
 
 var entJogador;
+var entObstaculo;
 
 function iniciarJogo(){
     espacoJogo.inicio();
-    entJogador = new proprJogador('#000', 50, 50, 50, 200);
+    entJogador = new proprComponente('#000', 50, 50, 50, 200);
+    entObstaculo = new proprComponente('#f00', 300, 20, 800, 100);
 }
 
 let espacoJogo = {
@@ -34,10 +49,13 @@ let espacoJogo = {
     },
     clearSpace: function(){
         this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+    },
+    stopGame: function(){
+        clearInterval(this.interval);
     }
 }
 
-function proprJogador(cor, largura, altura, x, y){
+function proprComponente(cor, largura, altura, x, y){
     this.altura = altura,
     this.largura = largura,
     this.x = x,
@@ -53,12 +71,39 @@ function proprJogador(cor, largura, altura, x, y){
         this.x += this.speedX;
         this.y += this.speedY;
     }
+
+    this.entCollision = function(ent){
+        //posição do personagem
+        let leftside = this.x;
+        let rightside = this.x + this.largura;
+        let topside = this.y;
+        let downside = this.y + this.altura;
+        //posição do obstaculo
+        let entLeftside = ent.x;
+        let entRightside = ent.x + ent.altura;
+        let entTopside = ent.y;
+        let entDownside = ent.y + ent.largura;
+    
+        let Collide = true;
+    
+        //verifica se NÃO bateu.
+        if((downside < entTopside) || (topside > entDownside) || (rightside < entLeftside) || (leftside > entRightside)){
+            Collide = false;
+        }
+    
+        return Collide;
+    } 
 }
 
 function updtEspacoJogo(){
+    if(entJogador.entCollision(entObstaculo)){
+        espacoJogo.stopGame();
+    }else{
     espacoJogo.clearSpace();
     entJogador.newPosition();
     entJogador.updtPosition();
+    entObstaculo.updtPosition();
+    }
 }
 
 function moveUp(){
@@ -77,3 +122,7 @@ function moveRight(){
     entJogador.speedX += 1;
 }
 
+function movemntBrake(){
+    entJogador.speedX = 0;
+    entJogador.speedY = 0;
+}    
